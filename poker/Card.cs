7025e4109f3242582ed5c0d34132cb1513173662
Aaron Hong <ahong02@uw.edu>
2023-03-28 22:54:14 -0700

    redesigning deck data structure

---
+++ poker/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poker
+{
+    internal class Card
+    {
+        string suit;
+        int value;
+
+        public Card(string suit, int value)
+        {
+            this.suit = suit;
+            this.value = value;
+        }
+    }
+}
